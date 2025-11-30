44d880afeca4d704d875bda7c4cf09bcb8f05f95
Gift Ilocie <gilocie@gmail.com>
2025-11-30 15:45:50 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/app/(app)/products/page.tsx
+++ src/app/(app)/products/page.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { useState } from 'react';
-import { useForm } from 'react';
+import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { useBrandsoft, type Product } from '@/hooks/use-brandsoft.tsx';
