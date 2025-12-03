56777be8977017b6007fc3a9757fee21cb83f987
Gift Ilocie <gilocie@gmail.com>
2025-12-03 16:40:09 +0000

    Like that?

--- src/lib/background-images.ts
+++ src/lib/background-images.ts
@@ -4,11 +4,10 @@
 // When you add a new image to the 'src/app/(app)/templates/backgrounds' folder,
 // add a new import line here and add the imported variable to the array below.
 
-import cert0 from '@/app/(app)/templates/backgrounds/brandsoft-cert0.jpg';
+import cert0 from '../app/(app)/templates/backgrounds/brandsoft-cert0.jpg';
 
 const backgroundImages: { name: string; src: any }[] = [
   { name: 'brandsoft-cert0', src: cert0 },
-  // { name: 'brandsoft-cert1', src: cert1 }, // Example for next image
 ];
 
 export default backgroundImages;
