2d21d7453775654cbd5a771500adce3da93a5259
Gift Ilocie <gilocie@gmail.com>
2025-12-09 06:57:11 +0000

    Now on Now since the images folder path is working properly, lets add mo

--- src/lib/background-images.ts
+++ src/lib/background-images.ts
@@ -5,12 +5,32 @@
 // To add a new image:
 // 1. Place the image file in the 'public/backgrounds' folder.
 // 2. Add a new entry to the array below with its public URL path.
-import brandsoftBackground1 from '@/app/(app)/templates/backgrounds/background.jpg';
 import invAndQuots0 from '@/app/(app)/templates/images/inv-and-quots0.jpg';
 
 
 const backgroundImages: { name: string; src: any }[] = [
   { name: 'inv-and-quots0', src: invAndQuots0 },
+  { name: 'inv-and-quots1', src: invAndQuots0 },
+  { name: 'inv-and-quots2', src: invAndQuots0 },
+  { name: 'inv-and-quots3', src: invAndQuots0 },
+  { name: 'inv-and-quots4', src: invAndQuots0 },
+  { name: 'inv-and-quots5', src: invAndQuots0 },
+  { name: 'inv-and-quots6', src: invAndQuots0 },
+  { name: 'inv-and-quots7', src: invAndQuots0 },
+  { name: 'inv-and-quots8', src: invAndQuots0 },
+  { name: 'inv-and-quots9', src: invAndQuots0 },
+  { name: 'inv-and-quots10', src: invAndQuots0 },
+  { name: 'inv-and-quots11', src: invAndQuots0 },
+  { name: 'inv-and-quots12', src: invAndQuots0 },
+  { name: 'inv-and-quots13', src: invAndQuots0 },
+  { name: 'inv-and-quots14', src: invAndQuots0 },
+  { name: 'inv-and-quots15', src: invAndQuots0 },
+  { name: 'inv-and-quots16', src: invAndQuots0 },
+  { name: 'inv-and-quots17', src: invAndQuots0 },
+  { name: 'inv-and-quots18', src: invAndQuots0 },
+  { name: 'inv-and-quots19', src: invAndQuots0 },
+  { name: 'inv-and-quots20', src: invAndQuots0 },
+  { name: 'inv-and-quots21', src: invAndQuots0 },
 ];
 
 export default backgroundImages;
