8f21b4e6a183a274e4ab663b33031ec2e683410f
Gift Ilocie <gilocie@gmail.com>
2025-12-09 06:34:45 +0000

    I want that image to apear on the galler for the user to choose when cus

--- src/lib/background-images.ts
+++ src/lib/background-images.ts
@@ -6,10 +6,12 @@
 // 1. Place the image file in the 'public/backgrounds' folder.
 // 2. Add a new entry to the array below with its public URL path.
 import brandsoftBackground1 from '@/app/(app)/templates/backgrounds/background.jpg';
+import invAndQuots0 from '@/app/(app)/templates/images/inv-and-quots0.jpg';
 
 
 const backgroundImages: { name: string; src: any }[] = [
   { name: 'background', src: brandsoftBackground1 },
+  { name: 'inv-and-quots0', src: invAndQuots0 },
 ];
 
 export default backgroundImages;
