dad9b878ba87a4a2536b0379bb23ae840fa05304
Your Name <gilocie@gmail.com>
2025-12-15 20:28:50 +0000

    Try fixing this error: `Runtime Error: ChunkLoadError: Loading chunk _ap

--- src/app/(app)/office/page.tsx
+++ src/app/(app)/office/page.tsx
@@ -1,21 +1,9 @@
 
 'use client';
 
-import dynamic from 'next/dynamic';
 import { Loader2 } from 'lucide-react';
 import { useBrandsoft } from '@/hooks/use-brandsoft';
-
-const OfficePageContent = dynamic(
-  () => import('./office-page-content').then((mod) => mod.OfficePageContent),
-  {
-    ssr: false,
-    loading: () => (
-      <div className="flex h-[80vh] w-full items-center justify-center">
-        <Loader2 className="h-12 w-12 animate-spin text-primary" />
-      </div>
-    ),
-  }
-);
+import { OfficePageContent } from './office-page-content';
 
 export default function OfficePage() {
   const { config } = useBrandsoft();
