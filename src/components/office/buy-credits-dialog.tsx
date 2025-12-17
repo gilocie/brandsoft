dd8dd55b3fef228bb5b16848a7c33292d4ad0c9b
Gift Ilocie <gilocie@gmail.com>
2025-12-17 15:51:34 +0000

    Try fixing this error: `Runtime Error: Error: DialogTrigger is not defin

--- src/components/office/buy-credits-dialog.tsx
+++ src/components/office/buy-credits-dialog.tsx
@@ -6,7 +6,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import { Button } from '@/components/ui/button';
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogFooter } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogFooter, DialogTrigger } from '@/components/ui/dialog';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { useToast } from '@/hooks/use-toast';
