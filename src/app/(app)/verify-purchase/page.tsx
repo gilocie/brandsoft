b70c6e8dc5a9464c176f815ae0fff5827bf56374
Gift Ilocie <gilocie@gmail.com>
2025-12-19 17:38:11 +0000

    Try fixing this error: `Runtime Error: Error: DialogTrigger is not defin

--- src/app/(app)/verify-purchase/page.tsx
+++ src/app/(app)/verify-purchase/page.tsx
@@ -18,7 +18,7 @@ import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog';
-import { Dialog, DialogContent, DialogHeader, DialogTitle as ShadcnDialogTitle, DialogFooter } from '@/components/ui/dialog';
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle as ShadcnDialogTitle, DialogFooter } from '@/components/ui/dialog';
 import { Textarea } from "@/components/ui/textarea";
 import { Progress } from "@/components/ui/progress";
 import { getReceiptFromDB } from '@/hooks/use-receipt-upload';
